197070c95627b5d437e06749a6f1983fc9ee5695
jamesreprise <james@ciner.is>
2022-05-30 12:25:06 +0100

    feat: line and station query

--- query.py
+++ query.py
@@ -1,6 +1,20 @@
 import psycopg2
 from psycopg2 import sql
 
+def station_query(station_name):
+    cur.execute(sql.SQL("""
+    SELECT name FROM lines WHERE line_id IN (SELECT line_id FROM station_lines WHERE station_id IN (SELECT station_id FROM stations WHERE name = %s));
+    """), [station_name])
+    return list(map(lambda x: x[0], cur.fetchall()))
+
+
+
+def line_query(line_name):
+    cur.execute(sql.SQL("""
+    SELECT name FROM stations WHERE station_id IN (SELECT station_id FROM station_lines WHERE line_id IN (SELECT line_id FROM lines WHERE name = %s));
+    """), [line_name])
+    return list(map(lambda x: x[0], cur.fetchall()))
+
 conn = psycopg2.connect("dbname=tubedb user=james password=london")
 cur = conn.cursor()
 
@@ -23,17 +37,10 @@ while True:
         query_type = query[0]
         query_data = query[1]
         if query_type == "station":
-            pass
+            print(station_query(query_data))
         elif query_type == "line":
-            pass
+            print(line_query(query_data))
         else:
             print("Your query wasn't of the formation 'station:name' or 'line:name'. Please try again.")
     else:
         print("Your query wasn't of the formation 'station:name' or 'line:name'. Please try again.")
-
-def station_query(station_name):
-
-    pass
-
-def line_query(line_name):
-    pass
